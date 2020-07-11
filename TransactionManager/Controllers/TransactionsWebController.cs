43251ce123d31ea42ec43e4c77baf6fada01fbdb
Yaroslav <zherebetskiy00@gmail.com>
2020-07-11 17:58:37 +0300

    added transaction controller; code cleanup

---
+++ TransactionManager/Controllers/TransactionsWebController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TransactionManager.Controllers
+{
+    [Route("transactions-web")]
+    public class TransactionsWebController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
